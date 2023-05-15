71ac9c26048b5b899d34343dc944c4f962d2d2bf
kraksy <kraksy8@gmail.com>
2023-05-15 16:16:09 +0200

    meh

--- THDRPC/THRPC.py
+++ THDRPC/THRPC.py
@@ -1,11 +1,40 @@
 from pypresence import Presence 
+import pypresence
 import time
+import pystray
+import PIL.Image
 import psutil
+import requests
+import sys
+import asyncio
+from websockets import server
+from websockets.sync.client import connect
 
 start = int(time.time())
-client_id = "1106637916203135048"
+client_id = ''
+oauth2token = ''
+
 RPC = Presence(client_id)  
 RPC.connect() 
+
+def getinfo():
+    c = pypresence.Client(client_id)
+    c.start()
+
+    auth = c.authorize(client_id, ['rpc'])
+    code_grant = auth.code
+
+    c.authenticate(oauth2token)
+    
+image = PIL.Image.open("THDRPC/logo.png")
+
+def on_clicked(icon, item):
+    icon.stop()
+    print("quit")
+
+icon = pystray.Icon("Neural", image, menu=pystray.Menu(
+    pystray.MenuItem("exit", on_clicked)
+))
 
 def TH19():
     RPC.update(
@@ -14,11 +43,16 @@ def TH19():
     details="Touhou 19",
     large_text="TH19",
     start=start
-    )
+)
 
+def clientID():
+    with connect("ws://127.0.0.1:6463/?v=VERSION&client_id=CLIENT_ID&encoding=ENCODING") as websocket:
+        cliid = websocket.recv()
 
 while True:  
-    
+    getinfo()
+    clientID()
+    icon.run()
     for proc in psutil.process_iter():
         match proc.name().lower():
             case "th19.exe":
@@ -26,8 +60,5 @@ while True:
 
             case _:
                 RPC.clear()
-
-    if TH19:
-
 
     time.sleep(15)
